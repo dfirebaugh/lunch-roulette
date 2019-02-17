951267c27bb8e08651f6c48b46d33a50cc6660c4
Dustin Firebaugh <dafirebaugh@gmail.com>
2019-02-16 19:08:37 -0500

    Updates

---
+++ precache-manifest.4b47890805d9e11bc25138242b376ef4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9706f6ba8176db041fef",
+    "url": "/lunch-roulette/static/js/runtime~main.9706f6ba.js"
+  },
+  {
+    "revision": "923666e0f95b225b964e",
+    "url": "/lunch-roulette/static/js/main.923666e0.chunk.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "/lunch-roulette/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "923666e0f95b225b964e",
+    "url": "/lunch-roulette/static/css/main.e3d14c65.chunk.css"
+  },
+  {
+    "revision": "c75bbebf1ef82256ec52c3467361ca1b",
+    "url": "/lunch-roulette/index.html"
+  }
+];

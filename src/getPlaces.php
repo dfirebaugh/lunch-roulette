c76da976b109be109f82a87c00b64836698f6942
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-02-28 21:01:49 -0500

    Built front-end button and connected to function in js file

--- src/getPlaces.php
+++ src/getPlaces.php
@@ -5,6 +5,7 @@
   // building url to fetch
   $baseurl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=';
   $key = getenv('API_KEY');
+  // $key = "removed";
   $location = "&location=" . $lat . "," . $lon;
   $radius = '&radius=3000&type=restaurant';
   $url = $baseurl . $key . $location . $radius;
